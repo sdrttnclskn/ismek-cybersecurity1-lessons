97a897a3b95509f1c34eb8d3d712e731e00554c5
root <root@cyberworks>
2017-11-03 16:26:11 -0400

    15-16 day_scripts

---
+++ bash_examples/forloop2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#for i in 0{1..9}{10..100}; do
+for i in $(ls /var/ ); do
+echo $i
+
+done
+
---
+++ bash_examples/while2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+count=6
+
+while [ $count -gt 0 ] ; do
+
+echo "degeri: $count "
+
+let count=count-1
+
+done
+
